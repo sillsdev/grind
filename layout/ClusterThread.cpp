b6ac2b4dab1adfe864b527e9d2e82bf7c95b3b32
Tim Eves <tim_eves@sil.org>
2014-06-30 11:00:43 +0700

    wip for cluster centric data structure
    Move a bunch per run methods onto the cluster thread class

--- layout/ClusterThread.cpp
+++ layout/ClusterThread.cpp
@@ -25,6 +25,10 @@ THE SOFTWARE.
 // Language headers
 // Interface headers
 #include "VCPlugInHeaders.h"
+#include "IJustificationStyle.h"
+#include "IWaxGlyphs.h"
+#include "IWaxRenderData.h"
+#include "IWaxRun.h"
 // Library headers
 // Module header
 #include "ClusterThread.h"
@@ -37,14 +41,90 @@ THE SOFTWARE.
 using namespace nrsc;
 
 inline
-cluster_thread::run cluster_thread::run::split(cluster_thread::base_t::iterator const & i)
+void cluster_thread::_span::split_into(base_t::iterator const & i, _span & rhs)
 {
-	run r(i,_e);
+	if (i == _b || i == _e) return;
 
-	_s -= r._s; 
-	_e = i; 
-	
-	return r;
+	rhs._b = i;
+	rhs._e = _e;
+	rhs._s = std::distance(i,_e);
+	_e = rhs._b;
+	_s -= rhs._s; 
+}
+
+
+
+size_t cluster_thread::_span::num_glyphs() const
+{
+	size_t n = 0;
+	for (const_iterator cl_i = begin(); cl_i != end(); ++cl_i)
+		n += cl_i->size();
+
+	return n;
+}
+
+
+IWaxRun * cluster_thread::run::wax_run() const
+{
+	// Check we've got composed text to put in the run and a line to put it in.
+	if (empty())	return nil;
+
+	// Create a new wax run object.
+	InterfacePtr<IWaxRun> wr(static_cast<IWaxRun*>(::CreateObject(kWaxTextRunBoss, IID_IWAXRUN)));
+	if (wr == nil)	return nil;
+
+	wr->SetYPosition(_ds->GetEffectiveBaseline());
+
+	// Fill out the render data for the run from the drawing style.
+	InterfacePtr<IWaxRenderData> rd(wr, UseDefaultIID());
+	InterfacePtr<IWaxGlyphs>     glyphs(wr, UseDefaultIID());
+	if (rd == nil || glyphs == nil)	return nil;
+
+	_ds->FillOutRenderData(rd, kFalse/*horizontal*/);
+	_ds->AddAdornments(wr);
+	wr->AddRef();
+
+	// Apply x skew.
+	PMMatrix glyphs_mat;
+	PMPoint pc;
+	glyphs_mat = glyphs->GetAllGlyphsMatrix(&pc);
+	glyphs_mat.SkewTo(_ds->GetSkewAngle());
+	glyphs_mat.Scale(1+_extra_scale, 1.0);
+	glyphs->SetAllGlyphsMatrix(glyphs_mat, pc);
+
+	_ops->render(*this, *glyphs);
+
+	return wr;
+}
+
+
+void cluster_thread::get_stretch_ratios(glyf::stretch & s) const
+{
+	PMReal min,des,max;
+	InterfacePtr<IJustificationStyle>	js(_runs.front().get_style(), UseDefaultIID());
+
+	s[glyf::fill].min = s[glyf::space].min;
+	s[glyf::fill].max = 1000000.0;
+	s[glyf::fill].num = 0;
+
+	js->GetWordspace(&min, &des, &max);
+	s[glyf::space].min = des - min;
+	s[glyf::space].max = max - des;
+	s[glyf::space].num = 0;
+
+	js->GetLetterspace(&min, &des, &max);
+	s[glyf::letter].min = des - min;
+	s[glyf::letter].max = max - des;
+	s[glyf::letter].num = 0;
+
+	js->GetGlyphscale(&min, &des, &max);
+	s[glyf::glyph].min = des - min;
+	s[glyf::glyph].max = max - des;
+	s[glyf::glyph].num = 0;
+
+	s[glyf::fixed].min = 0;
+	s[glyf::fixed].max = 0;
+	s[glyf::fixed].num = 0;
 }
 
 
@@ -53,15 +133,72 @@ cluster_thread cluster_thread::split(iterator const & i)
 	cluster_thread res;
 
 	// Split this cluster's run and push the rhs onto the result.
-	res._runs.push_back(i.run().split(i._cl));
-	if (res._runs.back().empty())	res.pop_back();
+	res._runs.push_back(i.run());
+	i.run().split_into(i, res._runs.back());
+	if (res._runs.back().empty())	res._runs.pop_back();
 
 	// Splice the runs after the cluster's run into the result
 	res._runs.splice(res._runs.end(), _runs, ++runs_t::iterator(i._r), _runs.end());
 	if (_runs.back().empty()) _runs.pop_back();
 
 	// Splice the clusters starting at i till the end into the result.
-	res.splice(res.base_t::end(), *this, i._cl, base_t::end());
+	res.splice(res.base_t::end(), *this, i, base_t::end());
 
 	return res;
 }
+
+void cluster_thread::trim_trailing_whitespace(const PMReal letter_space)
+{
+	InterfacePtr<IJustificationStyle>	js(_runs.front().get_style(), UseDefaultIID());
+
+	if (_trailing_ws_run == _runs.end())
+	{
+		// Find the last non-whitespace cluster
+		reverse_iterator	cl = rbegin();
+		for (const reverse_iterator cl_e = rend(); cl != cl_e; ++cl)
+			if (!cl->whitespace()) break;
+
+		if (cl == rend())
+		{
+			_trailing_ws_run = _runs.begin();
+			return;
+		}
+		_trailing_ws_run = cl.base()._r;
+	}
+
+	// Re-assign any letterspace contributed whitespace in the final
+	// glyph to the adjacent trailing whitespace.
+	if (_trailing_ws_run != _runs.end())
+	{
+		base_t::iterator non_ws = _trailing_ws_run->begin();
+		--non_ws;
+		non_ws->trim(letter_space);
+		_trailing_ws_run->begin()->front().kern(letter_space);
+	}
+}
+
+
+void cluster_thread::fit_trailing_whitespace(const PMReal margin)
+{
+	const size_t ws_count = std::distance(_trailing_ws_run->begin(), base_t::end());
+
+	// Shrink the trailing whitespace to fit into the margin space
+	const PMReal trailing_ws = margin/ws_count;
+	for (base_t::iterator cl = _trailing_ws_run->begin(), cl_e = base_t::end(); cl != cl_e; ++cl)
+		cl->front().kern(std::min(trailing_ws - cl->front().width(), PMReal(0)));
+}
+
+
+void cluster_thread::push_back(const value_type & val)
+{
+	base_t::iterator cl = insert(base_t::end(), val);
+	runs_t::iterator lr = --_runs.end();
+
+	if (lr->empty())
+	{
+		lr->_b = cl;
+		(--lr)->_e = cl;
+	}
+	++lr->_s;
+}
+
--- layout/ClusterThread.h
+++ layout/ClusterThread.h
@@ -28,69 +28,32 @@ THE SOFTWARE.
 #include <iterator>
 #include <list>
 // Interface headers
+#include <IDrawingStyle.h>
 // Library headers
 // Module header
 #include "Box.h"
 
 // Forward declarations
 // InDesign interfaces
-class IDrawingStyle;
+class IWaxGlyphs;
+class IWaxRun;
 // Graphite forward delcarations
 // Project forward declarations
 
 namespace nrsc
 {
 
+/*	cluster_thread
+	This class provides a cluster centric model of the paragraph or line, 
+	representing runs and spans pointing into the contiguous cluster list.
+*/
 class cluster_thread : private std::list<cluster>
 {
 	typedef std::list<cluster>	base_t;
 
 	template <typename C, typename R> class _iterator;
-	
-	class run
-	{
-		base_t::iterator _b, _e;
-		base_t::size_type _s;
-
-		// Modifiers
-		run split(base_t::iterator const &);
-
-	public:
-		run(base_t::iterator f, base_t::iterator l) : _b(f), _e(l), _s(std::distance(_b,_e)) {}
-
-		// Member types
-		typedef base_t::iterator				iterator;
-		typedef base_t::const_iterator			const_iterator;
-		typedef base_t::reverse_iterator		reverse_iterator;
-		typedef base_t::const_reverse_iterator	const_reverse_iterator;
-		typedef base_t::difference_type			difference_type;
-		typedef base_t::size_type				size_type;
-		typedef base_t::const_reference			const_reference;
-		typedef base_t::reference				reference;
-
-		// Iterators
-		iterator 		begin()					{ return _b; }
-		const_iterator	begin() const			{ return _b; }
-		iterator 		end()					{ return _e; }
-		const_iterator	end() const				{ return _e; }
-		reverse_iterator 		rbegin()		{ return reverse_iterator(_e); }
-		const_reverse_iterator	rbegin() const	{ return reverse_iterator(_e); }
-		reverse_iterator 		rend()			{ return reverse_iterator(_b); }
-		const_reverse_iterator	rend() const	{ return reverse_iterator(_b); }
-
-		// Capacity
-		bool		empty() const		{ return _s == 0; }
-		size_type	size() const		{ return _s; }
-
-		// Element access
-		reference front()				{ return *_b; }
-		const_reference front() const	{ return *_b; }
-		reference back()				{ iterator t = _e; --t; return *t; }
-		const_reference back() const	{ const_iterator t = _e; --t; return *t; }
-
-		friend cluster_thread;
-	};
-
+	class _span;
+	class run;
 
 public:
 	typedef	std::list<run>	runs_t;
@@ -115,7 +78,6 @@ public:
 	reverse_iterator 		rend();
 	const_reverse_iterator	rend() const;
 	
-
 	// Capacity
 	using base_t::empty;
 	using base_t::size;
@@ -125,13 +87,32 @@ public:
 	using base_t::back;
 
 	// Modifiers
-	cluster_thread split(iterator const &);
+	cluster_thread	split(iterator const &);
+	void			fit_trailing_whitespace(const PMReal margin);
+
+	// Properties
+	PMPoint			dimensions() const;
+	iterator		trailing_whitespace();
+	const_iterator	trailing_whitespace() const;
+
+	// Operations
+	void get_stretch_ratios(glyf::stretch & s) const;
+	void			trim_trailing_whitespace(const PMReal letter_space);
+	void			merge_runs();
+	void			open_run(IDrawingStyle * const ds, PMReal height);
+	void			push_back(const value_type & val);
 
 private:
-	runs_t		_runs;
+	runs_t				_runs;
+	runs_t::iterator	_trailing_ws_run;
 };
 
 
+
+/*	cluster_thread::_iterator
+	This nothing more than an iterator or the cluster list which also keeps 
+	track of the current clusters run.
+*/
 template<typename C, typename R>
 class cluster_thread::_iterator : public std::iterator<typename std::iterator_traits<C>::iterator_category,
 													   typename std::iterator_traits<C>::value_type>
@@ -149,18 +130,114 @@ public:
 	reference	operator*() const 		{ return _cl.operator*(); }
 	pointer 	operator->() const 		{ return _cl.operator->(); }
 
-	_iterator<C,R> & 	operator++() 		{ if (++_cl == r->end()) ++_r; return *this; }
+	_iterator<C,R> & 	operator++() 		{ if (++_cl == _r->end()) ++_r; return *this; }
 	_iterator<C,R> & 	operator++(int) 	{ _iterator<C,R> tmp = *this; operator++(); return tmp; }
 
-	_iterator<C,R> & 	operator--()		{ if (_cl == r->begin()) --_r; --_cl; return *this; }
+	_iterator<C,R> & 	operator--()		{ if (_cl == _r->begin()) --_r; --_cl; return *this; }
 	_iterator<C,R> & 	operator--(int)		{ _iterator<C,R> tmp = *this; operator--(); return tmp; }
 
 	typename R::reference	run() const			{ return *_r; }
 
-	friend cluster_thread cluster_thread::split(cluster_thread::iterator const &);
+	operator C () const { return _cl; }
+
+	friend class cluster_thread;
 };
 
 
+
+/*	cluster_thread::_span
+	A span is simply a sub sequence of the cluster list which masquerades as a
+	container.
+*/
+class cluster_thread::_span
+{
+	base_t::iterator _b, _e;
+	base_t::size_type _s;
+
+	virtual void split_into(base_t::iterator const &, _span &);
+
+	friend cluster_thread;
+public:
+	_span(base_t::iterator f, base_t::iterator l) : _b(f), _e(l), _s(std::distance(_b,_e)) {}
+
+	// Member types
+	typedef base_t::iterator				iterator;
+	typedef base_t::const_iterator			const_iterator;
+	typedef base_t::reverse_iterator		reverse_iterator;
+	typedef base_t::const_reverse_iterator	const_reverse_iterator;
+	typedef base_t::difference_type			difference_type;
+	typedef base_t::size_type				size_type;
+	typedef base_t::const_reference			const_reference;
+	typedef base_t::reference				reference;
+
+	// Iterators
+	iterator 		begin()					{ return _b; }
+	const_iterator	begin() const			{ return _b; }
+	iterator 		end()					{ return _e; }
+	const_iterator	end() const				{ return _e; }
+	reverse_iterator 		rbegin()		{ return reverse_iterator(_e); }
+	const_reverse_iterator	rbegin() const	{ return reverse_iterator(_e); }
+	reverse_iterator 		rend()			{ return reverse_iterator(_b); }
+	const_reverse_iterator	rend() const	{ return reverse_iterator(_b); }
+
+	// Capacity
+	bool		empty() const		{ return _s == 0; }
+	size_type	size() const		{ return _s; }
+	size_type	span() const;
+	size_type	num_glyphs() const;
+
+
+	// Element access
+	reference front()				{ return *_b; }
+	const_reference front() const	{ return *_b; }
+	reference back()				{ iterator t = _e; --t; return *t; }
+	const_reference back() const	{ const_iterator t = _e; --t; return *t; }
+};
+
+
+
+/*	cluster_thread::run
+	Ultimately represents an InDesign WaxRun object, this is a span which 
+	knows it's clusters IDrawingStyle, scaling info and other geometry.
+*/
+class cluster_thread::run : public cluster_thread::_span
+{
+	InterfacePtr<IDrawingStyle>	_ds;
+	PMReal						_extra_scale,
+								_h;
+
+protected:
+	struct run_ops
+	{
+		virtual bool render(cluster_thread::run const & run, IWaxGlyphs & glyphs);
+	};
+
+	static run_ops ops;
+	run_ops * _ops;
+
+public:
+	run(IDrawingStyle * const ds, PMReal height, base_t::iterator f, base_t::iterator l);
+
+	// Operations
+	bool			joinable(run const & rhs) const;
+	run	&			join(run & rhs);
+	
+	PMReal			width() const;
+	PMReal			height() const;
+
+	IWaxRun		  * wax_run() const;
+	IDrawingStyle * get_style() const;
+
+	void get_stretch_ratios(glyf::stretch &) const;
+	void calculate_stretch(glyf::stretch const & js, glyf::stretch & s) const;
+	void apply_desired_widths();
+	void adjust_widths(PMReal fill_space, PMReal word_space, PMReal letter_space, PMReal glyph_scale);
+};
+
+
+
+/* Inline functions for cluster_thread
+*/
 inline
 cluster_thread::iterator cluster_thread::begin()
 {
@@ -207,6 +284,67 @@ inline
 cluster_thread::const_reverse_iterator cluster_thread::rend() const
 {
 	return const_reverse_iterator(begin());
+}
+
+inline
+cluster_thread::iterator cluster_thread::trailing_whitespace()
+{
+	return iterator(_trailing_ws_run->begin(), _trailing_ws_run);
+}
+
+inline
+cluster_thread::const_iterator cluster_thread::trailing_whitespace() const
+{
+	return const_iterator(_trailing_ws_run->begin(), _trailing_ws_run);
+}
+
+inline
+void cluster_thread::open_run(IDrawingStyle * const ds, PMReal height)
+{
+	_runs.push_back(run(ds, height, base_t::end(), base_t::end()));
+}
+
+
+
+/* Inline functions for cluster_thread::run
+*/
+inline
+cluster_thread::run::run(IDrawingStyle * const ds, PMReal height, base_t::iterator f, base_t::iterator l) 
+: cluster_thread::_span(f,l), _ds(ds), _h(height), _ops(&ops) 
+{
+}
+
+inline
+bool cluster_thread::run::joinable(const run & rhs) const
+{
+	return rhs._ds->CanShareWaxRunWith(rhs._ds);
+}
+
+inline
+cluster_thread::run & cluster_thread::run::join(cluster_thread::run & rhs) 
+{
+	_e = rhs._e; _s += rhs._s;
+	return *this;
+}
+
+inline
+PMReal cluster_thread::run::width() const
+{
+	PMReal advance = 0;
+	for (const_iterator cl = begin(), cl_e = end(); cl != end(); advance += cl->width(), ++cl);
+	return advance;
+}
+
+inline
+PMReal cluster_thread::run::height() const
+{
+	return _h;
+}
+
+inline
+IDrawingStyle  * cluster_thread::run::get_style() const
+{
+	return _ds;
 }
 
 } // End of namespace nrsc
